b736e1620c5d7478239772c5e020b722f3d3ff33
Brunaco <correabruno412@gmail.com>
2020-04-07 21:16:30 -0300

    Control de velocidad por botones

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <Arduino.h>
-const int rojo = 0;
-const int amarillo= 1;
-const int verde = 2;
-const int verde2 = 3;
-const int amarillo2= 4;
-const int rojo2 = 5;
-int tiempo;
+const int rojo = 8;
+const int amarillo= 9;
+const int verde = 10;
+const int verde2 = 11;
+const int amarillo2= 12;
+const int rojo2 = 13;
+
+volatile int tiempo = 1000; 
+
+void mas(){
+  delay(20);
+  tiempo++;
+}
+void menos(){
+  tiempo--;
+  delay(20);
+}
  
 void setup() {
   pinMode(rojo, OUTPUT);
@@ -15,6 +25,7 @@ void setup() {
   pinMode(amarillo2, OUTPUT);
   pinMode(rojo2, OUTPUT);
 
+
   digitalWrite(rojo, LOW);
   digitalWrite(amarillo, LOW);
   digitalWrite(verde, LOW);
@@ -22,38 +33,41 @@ void setup() {
   digitalWrite(amarillo2, LOW);
   digitalWrite(verde2, LOW);
   
+  attachInterrupt(digitalPinToInterrupt(2), mas, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), menos, RISING);
+
 }
 
 void loop() {
   digitalWrite(rojo, HIGH);
-  delay(100);
+  delay(tiempo);
   digitalWrite(rojo,LOW);
   digitalWrite(amarillo, HIGH);
-  delay(100);
+  delay(tiempo);
   digitalWrite(amarillo,LOW);
   digitalWrite(verde, HIGH);
-  delay(100);
+  delay(tiempo);
   digitalWrite(verde,LOW);
   digitalWrite(verde2, HIGH);
-  delay(100);
+  delay(tiempo);
   digitalWrite(verde2,LOW);
   digitalWrite(amarillo2, HIGH);
-  delay(100);
+  delay(tiempo);
   digitalWrite(amarillo2,LOW);
   digitalWrite(rojo2, HIGH);
-  delay(100);
+  delay(tiempo);
   digitalWrite(rojo2,LOW);
   digitalWrite(amarillo2, HIGH);
-  delay(100);
+  delay(tiempo);
   digitalWrite(amarillo2,LOW);
   digitalWrite(verde2, HIGH);
-  delay(100);
+  delay(tiempo);
   digitalWrite(verde2,LOW);
   digitalWrite(verde, HIGH);
-  delay(100);
+  delay(tiempo);
   digitalWrite(verde,LOW);
   digitalWrite(amarillo, HIGH);
-  delay(100);
+  delay(tiempo);
   digitalWrite(amarillo,LOW);
 
 }
